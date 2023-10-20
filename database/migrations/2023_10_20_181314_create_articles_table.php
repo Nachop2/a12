7e16bc5a39081a195de760d6760288cdd1f9d7a9
Nacho <italiyankina@gmail.com>
2023-10-20 19:44:54 +0100

    fix Articles

--- database/migrations/2023_10_20_181314_create_articles_table.php
+++ database/migrations/2023_10_20_181314_create_articles_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->unsignedBigInteger('user_id'); //Foránea
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
